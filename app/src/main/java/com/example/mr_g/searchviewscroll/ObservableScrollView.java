dfc03a202b64959f3484c2a167cbcf2f510d4d03
Mr_gao <787080436@qq.com>
2016-03-30 20:58:10 +0800

    代码优化

--- app/src/main/java/com/example/mr_g/searchviewscroll/ObservableScrollView.java
+++ app/src/main/java/com/example/mr_g/searchviewscroll/ObservableScrollView.java
@@ -6,27 +6,29 @@ import android.widget.ScrollView;
 
 /**
  * Created by Mr_g on 16/3/30.
+ * scrollVie 没有对滑动的监听。定义一个接口把数据暴露出去。
  */
 public class ObservableScrollView extends ScrollView {
 
+
     private ScrollViewListener scrollViewListener = null;
+
 
     public ObservableScrollView(Context context) {
         super(context);
     }
-
-    public ObservableScrollView(Context context, AttributeSet attrs,
-                                int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
     public ObservableScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+    public ObservableScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
 
     public void setScrollViewListener(ScrollViewListener scrollViewListener) {
         this.scrollViewListener = scrollViewListener;
     }
+
 
     @Override
     protected void onScrollChanged(int x, int y, int oldx, int oldy) {
--- app/src/main/java/com/example/mr_g/searchviewscroll/SearchViewActivity.java
+++ app/src/main/java/com/example/mr_g/searchviewscroll/SearchViewActivity.java
@@ -1,23 +1,16 @@
 package com.example.mr_g.searchviewscroll;
 
-import android.animation.ArgbEvaluator;
 import android.animation.IntEvaluator;
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 
@@ -30,12 +23,11 @@ public class SearchViewActivity extends AppCompatActivity {
     private static final float ENDMARGINTOP = 5;
     private static final float STARTMARGINLEFT = 20;
     private static final float STARTMARGINTOP = 140;
+    private RelativeLayout rv_bar;
     private RelativeLayout rv_search;
-    private RelativeLayout search_rv;
-    private ObservableScrollView sv_search;
-    
+
     private ImageView iv_search;
-    private int result;
+    private int scrollLength;//顶部栏从透明变成不透明滑动的距离
 
 
 
@@ -45,83 +37,80 @@ public class SearchViewActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_searchview);
 
-        init();
 
 
-    }
+            rv_bar = (RelativeLayout) findViewById(R.id.rv_bar);
+            rv_search = (RelativeLayout) findViewById(R.id.rv_search);
+        ObservableScrollView sv_search = (ObservableScrollView) findViewById(R.id.sv_search);
+            iv_search = (ImageView) findViewById(R.id.iv_search);
+            MyListview lv_searchview = (MyListview) findViewById(R.id.lv_searchview);
 
 
-    @TargetApi(Build.VERSION_CODES.M)
-    private void init() {
-        rv_search = (RelativeLayout) findViewById(R.id.rv_search);
-        search_rv = (RelativeLayout) findViewById(R.id.search_rv);
+            String[] strings = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",};
+            lv_searchview.setAdapter(new searchAdapter(SearchViewActivity.this, strings));
+            sv_search.smoothScrollTo(0, 0);
 
-        sv_search = (ObservableScrollView) findViewById(R.id.sv_search);
-        iv_search = (ImageView) findViewById(R.id.iv_search);
-        MyListview lv_searchview = (MyListview) findViewById(R.id.lv_searchview);
+            sv_search.setScrollViewListener(new ScrollViewListener() {
 
-        String[] strings = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",};
-        lv_searchview.setAdapter(new searchAdapter(SearchViewActivity.this, strings));
-        sv_search.smoothScrollTo(0, 0);
+                private int evaluatemargin;
+                private int evaluatetop;
+                private FrameLayout.LayoutParams layoutParams;
 
-        sv_search.setScrollViewListener(new ScrollViewListener() {
+                @Override
+                public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
 
-            private int evaluatemargin;
-            private int evaluatetop;
-            private FrameLayout.LayoutParams layoutParams;
-            @Override
-            public void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy) {
-                Log.e("gaoyiming", "x" + x + "y" + y);
+                    int abs_y = Math.abs(y);
+                    //滑动距离小于顶部栏从透明到不透明所需的距离
+                    if ((scrollLength - abs_y) > 0) {
 
-                int abs_y = Math.abs(y);
-                if ((result - abs_y) > 0) {
+                        //估值器
+                        IntEvaluator evaluator = new IntEvaluator();
+                        float percent = (float) (scrollLength - abs_y) / scrollLength;
+                        
+                        if (percent <= 1) {
+
+                            //透明度
+                            int evaluate = evaluator.evaluate(percent, 255, 0);
+                            rv_bar.getBackground().setAlpha(evaluate);
+                            //搜索栏左右margin值
+                            evaluatemargin = evaluator.evaluate(percent, DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT), DensityUtil.dip2px(SearchViewActivity.this,STARTMARGINLEFT));
+                            //搜索栏顶部margin值
+                            evaluatetop = evaluator.evaluate(percent,  DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINTOP), DensityUtil.dip2px(SearchViewActivity.this,STARTMARGINTOP));
+
+                            layoutParams = (FrameLayout.LayoutParams) rv_search.getLayoutParams();
+                            layoutParams.setMargins(evaluatemargin, evaluatetop, evaluatemargin, 0);
+                            rv_search.requestLayout();
+                        }
 
 
-                    IntEvaluator evaluator = new IntEvaluator();
-                    float v = (float) (result - abs_y) / result;
-                    Log.e("v", "" + v);
-                    if (v <= 1) {
-                        int evaluate = (Integer) evaluator.evaluate(v, 255, 0);
-                        Log.e("evaluate", evaluate + "");
-                        rv_search.getBackground().setAlpha(evaluate);
-
-                        evaluatemargin = (Integer) evaluator.evaluate(v, DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT), DensityUtil.dip2px(SearchViewActivity.this,STARTMARGINLEFT));
-                        evaluatetop = (Integer) evaluator.evaluate(v,  DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINTOP), DensityUtil.dip2px(SearchViewActivity.this,STARTMARGINTOP));
-                        layoutParams = (FrameLayout.LayoutParams) search_rv.getLayoutParams();
-                        layoutParams.setMargins(evaluatemargin, evaluatetop, evaluatemargin, 0);
-                        search_rv.requestLayout();
+                    } else {
+                        rv_bar.getBackground().setAlpha(255);
+                        if(layoutParams!=null){
+                            layoutParams.setMargins(DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT),DensityUtil.dip2px(SearchViewActivity.this,5), DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT), 0);
+                            rv_search.requestLayout();
+                        }
 
                     }
-
-
-                } else {
-                    rv_search.getBackground().setAlpha(255);
-                    if(layoutParams!=null){
-                        layoutParams.setMargins(DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT),DensityUtil.dip2px(SearchViewActivity.this,5), DensityUtil.dip2px(SearchViewActivity.this,ENDMARGINLEFT), 0);
-                        search_rv.requestLayout();
-                    }
-
                 }
-            }
-        });
+            });
+
+
+
     }
+
+
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-
-
         super.onWindowFocusChanged(hasFocus);
 
-        int height_rv = rv_search.getHeight();
-        int measuredHeight = rv_search.getMeasuredHeight();
-        int bottom = rv_search.getBottom();
-
+        int height_rv = rv_bar.getHeight();
         int height_iv = iv_search.getHeight();
-        int measuredHeight1 = iv_search.getMeasuredHeight();
-        int bottom1 = iv_search.getBottom();
-        Log.e("gaoyiming2", "x" + height_iv + measuredHeight1 + bottom1);
-        result = Math.abs(height_iv - height_rv);
-        rv_search.getBackground().setAlpha(0);
+
+        scrollLength = Math.abs(height_iv - height_rv);
+
+        //把顶部bar设置为透明
+        rv_bar.getBackground().setAlpha(0);
 
     }
 
@@ -158,10 +147,9 @@ public class SearchViewActivity extends AppCompatActivity {
                 tv_search = (TextView) view.findViewById(R.id.tv_search);
             } else {
                 view = convertView;
-
             }
 
-            tv_search.setText("1");
+            tv_search.setText(strings[position]);
             return view;
         }
     }
